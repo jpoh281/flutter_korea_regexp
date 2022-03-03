5ea8e3c320ffc5ce8a933211a8cd79bbd7d78b99
jpoh281 <hjp4869@naver.com>
2022-03-04 00:46:42 +0900

    📦 export 변경

--- lib/korea_regexp.dart
+++ lib/korea_regexp.dart
@@ -1,1 +1,8 @@
-library korea_regexp;
+
+export 'package:korea_regexp/src/models/phonemes_result.dart';
+export 'package:korea_regexp/src/models/regexp_options.dart';
+export 'package:korea_regexp/src/get_phonemes.dart';
+export 'package:korea_regexp/src/get_regexp.dart' show getRegExp;
+export 'package:korea_regexp/src/kor_to_eng.dart' show korToEng;
+export 'package:korea_regexp/src/explode.dart';
+export 'package:korea_regexp/src/escape_regexp.dart';
