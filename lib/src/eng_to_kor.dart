d22c931542d1a86be37faf0ba4b2a25a1c177e17
viiviii <viiiix.viiviii@gmail.com>
2022-03-21 22:40:57 +0900

    :sparkles: 영타 → 한타 기능

---
+++ lib/src/eng_to_kor.dart
@@ -0,0 +1,10 @@
+import 'package:korea_regexp/src/constant.dart';
+import 'package:korea_regexp/src/implode.dart';
+
+final EN_TO_KR = Map<String, String>.fromIterable(KEYS,
+    key: (e) => e.last, value: (e) => e.first);
+
+String engToKor(String eng) {
+  final kor = eng.split('').map((char) => EN_TO_KR[char] ?? char).join();
+  return implode(kor);
+}
