ca1d4fe30516fd2204e82adc025bec6508e11bc9
홍종표 <54665433+jpoh281@users.noreply.github.com>
2022-08-29 22:44:49 +0900

    🧹 린트에 맞게 코드 수정 (#27)

--- lib/src/eng_to_kor.dart
+++ lib/src/eng_to_kor.dart
@@ -1,10 +1,9 @@
 import 'package:korea_regexp/src/constant.dart';
 import 'package:korea_regexp/src/implode.dart';
 
-final EN_TO_KR = Map<String, String>.fromIterable(keys,
-    key: (e) => e.last, value: (e) => e.first);
+final enToKr = { for (var e in keys) e.last : e.first };
 
 String engToKor(String eng) {
-  final kor = eng.split('').map((char) => EN_TO_KR[char] ?? char).join();
+  final kor = eng.split('').map((char) => enToKr[char] ?? char).join();
   return implode(kor);
 }
