c6d3f677d42c1eef9fe6b4ee36b9c292c7f74b03
Jinho Song <36188961+aijinho@users.noreply.github.com>
2025-07-02 10:48:23 +0900

    change escape character replacement
    escape 문자가 포함되었을 경우 \ 포함하는 수정입니다

--- lib/src/escape_regexp.dart
+++ lib/src/escape_regexp.dart
@@ -1,8 +1,9 @@
-String reRegExpChar = '[\\^\$.*+?()[\]{}|]';
+String reRegExpChar = r'[\\^\$.*+?\(\)\[\]\{\}|]';
 RegExp reHasRegExpChar = RegExp(reRegExpChar);
 
 escapeRegExp(String string) {
   return string.isNotEmpty && reHasRegExpChar.hasMatch(string)
-      ? string.replaceAll(reRegExpChar, '\\\$&')
+      ? string.replaceAllMapped(
+          reHasRegExpChar, (match) => '\\${match.group(0)}')
       : string;
 }
